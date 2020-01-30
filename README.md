f51640e5db9781e5615b307c903096c980c9160d
Roi Otero Morales <roi.otero.morales@infinityworks.com>
2020-01-30 13:05:13 +0000

    Add docker

--- README.md
+++ README.md
@@ -2,4 +2,20 @@
 
 For a graphical way of viewing builds and promoting them
 
+## How to run it
+Just start the spring boot project and will be under the url: 
+
 http://localhost:8080/env-status
+
+### Docker
+
+docker run -e DRONE_SERVER=$DRONE_SERVER -e DRONE_TOKEN=$DRONE_TOKEN -e ORGANIZATION=$ORGANIZATION -e GITHUB_TOKEN=$GITHUB_TOKEN roioteromorales/build-status:latest 
+
+### Requirements:
+Add to your environment variables these values:
+
+  - DRONE_TOKEN ex: 123456789abcd
+  - DRONE_SERVER ex: https://ci.build.yourcompany.com
+  - ORGANIZATION ex: roisoftstudio
+  - GITHUB_TOKEN ex: 123456789abcd
+
--- src/main/java/com/roisoftstudio/buildstatus/logic/BlacklistChecker.java
+++ src/main/java/com/roisoftstudio/buildstatus/logic/BlacklistChecker.java
@@ -1,35 +1,26 @@
 package com.roisoftstudio.buildstatus.logic;
 
-import static java.util.stream.Collectors.toList;
+import static java.util.Arrays.asList;
 
 import com.roisoftstudio.buildstatus.data.dto.DroneRepo;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
+import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class BlacklistChecker {
 
-  private static final String FILENAME = "blacklist-repos";
-  private List<String> blacklist = new ArrayList<>();
+  @Value("${blacklist}")
+  private String[] blacklistArray;
+  private List<String> blacklist;
 
-  public BlacklistChecker() {
-    try {
-      Path path = Paths.get(getClass().getClassLoader().getResource(FILENAME).toURI());
-      Stream<String> lines = Files.lines(path);
-      blacklist = lines.collect(toList());
-      lines.close();
-    } catch (URISyntaxException | IOException e) {
-      log.error("There was an error reading the blacklist " + FILENAME, e);
-    }
+  @PostConstruct
+  public void initializeFile() {
+    blacklist = asList(blacklistArray);
+    log.info("BlackList repos: {}", blacklist);
   }
 
   public boolean isBlacklisted(DroneRepo droneRepo) {
