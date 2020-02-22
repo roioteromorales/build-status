1e2b38bfb2ca50127645694f307616c8d58e8fb8
Roi Otero Morales <roi.otero.morales@infinityworks.com>
2020-02-22 13:07:19 +0000

    Improve search

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -6,8 +6,25 @@ var promote = async (repo, buildNumber, environment) => {
       'Content-Type': 'application/json'
     }
   });
-  const myJson = await response.json(); //extract JSON from the http response
+  const myJson = await response.json();
   console.log("received response: ");
   console.log(myJson);
   location.reload();
+}
+
+function searchRepo() {
+  var input, filter, row, rows, i, txtValue;
+  input = document.getElementById('search');
+  filter = input.value.toUpperCase();
+  rows = document.getElementsByClassName('versionRow');
+
+  for (i = 0; i < rows.length; i++) {
+    row = rows[i].getElementsByTagName("h5")[0]
+    txtValue = row.textContent || row.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      rows[i].style.display = "";
+    } else {
+      rows[i].style.display = "none";
+    }
+  }
 }
